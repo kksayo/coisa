e06cc0b7205d83e60547f1e9ca1675588c9c1e59
kksayo <dudgabriel@hotmail.com>
2024-05-10 20:56:31 -0300

    Carros2

--- resources/views/cria-conta.blade.php
+++ resources/views/cria-conta.blade.php
@@ -1,35 +1,36 @@
 <x-layout_base>
 <!-- Hero -->
-<div class="absolute"><img src="https://i.pinimg.com/originals/a4/d8/73/a4d873fa9d727b78e5aaf9c582ace9e7.jpg" alt=""></div>
-<div class="relative bg-gradient-to-b from-yellow-500 via--500 to-white-500">
+<div class="absolute ml-96"><img src="https://wallpaper.forfun.com/fetch/04/0478f5b6363ef6f93e8b22cd2267d874.jpeg" alt=""></div>
+<div class="relative items-end bg-gradient-to-b from-red-500 via--500 to-black-500">
+<div class="relative items-end bg-red-500 w-1/2">
   <div class="flex flex-col items-end justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 mr-64 ">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-12 h-12 mr-2" src="https://i.pinimg.com/originals/ab/15/1d/ab151d4a39befc3479530467ee2a769a.png" alt="logo">
+          <img class="w-16 h-12 mr-2" src="https://i.pinimg.com/originals/b5/74/04/b574045f483218530f7f284802a03ec7.png" alt="logo">
             <div class="mr-32">
-          Mercado Minion
+          Carros Market
 </div>
       </a>
-      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-yellow-200  dark:border-gray-700">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-red-300  dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-blue-300">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-yellow-300">
                   Faça seu login, fofinho!
               </h1>
               <form action="{{route('salva-usuario')}}" method="POST">
                 @csrf
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-300">Your User</label>
-                      <input type="text" name="nome" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="SeuNomeMinion" required="">
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-yellow-300">Your User</label>
+                      <input type="text" name="nome" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="SeuNomeMinion" required="">
                   </div>
 
                   
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-300">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="minionname@company.com" required="">
+                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-yellow-300">Your email</label>
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="minionname@company.com" required="">
                   </div>
                   
                   <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-300">Password</label>
-                      <input type="password" name="senha" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-blue-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-yellow-300">Password</label>
+                      <input type="password" name="senha" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
@@ -37,14 +38,14 @@
                             <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 ml-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-blue-200 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-white" required="">
                           </div>
                           <div class="ml-3 text-sm">
-                            <label for="remember" class="dark:text-blue-500">Remember me</label>
+                            <label for="remember" class="dark:text-yellow-500">Remember me</label>
                           </div>
                       </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-blue-500">Forgot password?</a>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-yellow-500">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-blue-500">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-blue-500">Sign up</a>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-yellow-500">Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-blue-500">
+                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-yellow-500">Sign up</a>
                   </p>
               </form>
           </div>
@@ -53,6 +54,7 @@
 </section> 
 
 
+</div>
 </div>
 </div>
 <!-- End Hero -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,21 @@
 <x-layout_base>
-<div class="navbar bg-yellow-300">
+<div class="navbar bg-red-300">
   <div class="flex-1">
-    <a class="btn btn-ghost text-black text-xl">Mercado Livre</a>
+    <a class="btn btn-ghost text-black text-xl">Carros Market</a>
   </div>
   <div class="flex-none gap-3">
-    <div class="form-control">
-      <input type="text" placeholder="Search" class="input input-bordered md:w-auto bg-white" />
+    <div class="form-control mr-[20rem] w-[64rem]">
+      <input type="text" placeholder="Search" class="input input-bordered md:w-auto bg-red-100" />
     </div>
     <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <img alt="Tailwind CSS Navbar component" src="https://recreio.uol.com.br/media/uploads/2024/02/mate-carros_capa.jpg" />
         </div>
       </div>
-      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content text-black rounded-box w-52 bg-red-300">
         <li>
-          <a class="justify-between">
+          <a class="justify-between bg-red-300">
             Profile
             <span class="badge">New</span>
           </a>
@@ -28,16 +28,16 @@
 </div>
 
 
-<div class="navbar bg-yellow-300">
+<div class="navbar bg-red-300">
   <div class="navbar-start text-black text-xl ml-10">
     <div class="dropdown">
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-red-300 rounded-box w-52">
         <li>
           <summary>Categorias</summary>
-          <ul class="p-2">
+          <ul class="p-2 bg-red-300">
             <li><a>Veiculos</a></li>
             <li><a>Supermercado</a></li>
             <li><a>Tecnologia</a></li>
@@ -53,12 +53,12 @@
     </div>
     <a class="btn btn-ghost text-xl"></a>
   </div>
-  <div class="navbar-center hidden z-40 lg:flex bg-yellow-300 text-black">
+  <div class="navbar-center hidden z-40 lg:flex bg-red-300 text-black">
     <ul class="menu menu-horizontal px-1">
       <li>
         <details>
           <summary>Categorias</summary>
-          <ul class="p-2">
+          <ul class="p-2 bg-red-300">
             <li><a>Veiculos</a></li>
             <li><a>Supermercado</a></li>
             <li><a>Tecnologia</a></li>
@@ -73,7 +73,7 @@
     </ul>
   </div>
   <div class="navbar-end">
-    <a class="btn bg-yellow-300 text-black hover:bg-yellow-100">Compras</a>
+    <a class="btn bg-red-300 text-black hover:bg-red-100">Compras</a>
   </div>
 </div>
 
